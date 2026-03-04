8b3fdd1acd4147e66e8ab82b61cb769bef8cf69c
Emerson Jesus <emersomjesus999@gmail.com>
2026-03-03 23:13:45 -0300

    Create ObjetoNaoEncontradoException.java

---
+++ 06-Fila-de-Supermercado-com-tempo-de-chegada/ObjetoNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package atv8_filaMercado;
+
+public class ObjetoNaoEncontradoException extends Exception {
+	 public ObjetoNaoEncontradoException() {
+	        super("O objeto não foi encontrado na lista!");
+	    }
+
+}
