f2322e2e9410a7bf350f885bab42c4e5f3b342fe
Emerson Jesus <emersomjesus999@gmail.com>
2026-03-03 22:17:03 -0300

    Create ObjetoNaoEncontradoException.java

---
+++ 01-lista-encadeada/ObjetoNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package atv2_listaSimplesmenteEncadeada;
+
+public class ObjetoNaoEncontradoException extends Exception {
+	 public ObjetoNaoEncontradoException() {
+	        super("O objeto não foi encontrado na lista!");
+	    }
+
+}
