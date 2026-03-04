ac2c8ce1e005b5dcf27693c3e2d57b16b7345651
Emerson Jesus <emersomjesus999@gmail.com>
2026-03-03 23:02:16 -0300

    Create ObjetoNaoEncontradoException.java

---
+++ algoritmos-para-transformação-de-expressoes-artimeticas/ObjetoNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package atv5_transformacaoDeExpressoes;
+
+public class ObjetoNaoEncontradoException extends Exception {
+	 public ObjetoNaoEncontradoException() {
+	        super("O objeto não foi encontrado na lista!");
+	    }
+
+}
