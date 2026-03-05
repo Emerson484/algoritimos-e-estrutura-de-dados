f0f4f27141e4fafb93e260942323b614acfd6e83
Emerson Jesus <emersomjesus999@gmail.com>
2026-03-04 21:20:21 -0300

    Create IPilha.java

---
+++ 07-arvore-binaria/IPilha.java
@@ -0,0 +1,10 @@
+package atv11_ArvoreBinaria;
+
+public interface IPilha<T> {
+	void fazVazia();
+	boolean estaVazia();
+	T getTop () throws ContainerVazioException,ListaVaziaException;
+	void push(T object);
+	T pop() throws ContainerVazioException,ObjetoNaoEncontradoException,ListaVaziaException;
+
+}
