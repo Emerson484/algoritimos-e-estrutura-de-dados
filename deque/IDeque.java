db0f204f695220e39ad1cd2c054c3b4f6eea5704
Emerson Jesus <emersomjesus999@gmail.com>
2026-03-04 21:57:47 -0300

    Create IDeque.java

---
+++ deque/IDeque.java
@@ -0,0 +1,18 @@
+package atv7_Deque;
+
+import atv6_filaComArray.ContainerCheioException;
+import atv6_filaComArray.ContainerVazioException;
+
+public interface IDeque <T>{
+	void fazVazia();
+	boolean estaVazia();
+	T getPrimeiro()throws ContainerVazioException;
+	T getUltimo()throws ContainerVazioException;
+	void enfileirarInicio(T object)throws ContainerCheioException;
+	void enfileirarFim(T object)throws ContainerCheioException;
+	T desinfileirarInicio() throws ContainerVazioException;
+	T desinfileirarFim()throws ContainerVazioException ;
+
+	
+
+}
