3e3bcdf0baed0c54e82cb577a4b0533677fc8832
Emerson Jesus <emersomjesus999@gmail.com>
2026-03-04 21:31:07 -0300

    Create FilaEncadeada.java

---
+++ 08-arvore-binaria-de-busca/FilaEncadeada.java
@@ -0,0 +1,44 @@
+package atv11_ArvoreBinariaDeBusca;
+
+public class FilaEncadeada<T> implements IFila<T>{
+	private ListaEncadeadaGenerica <T> lista;
+	private int count;
+	
+	public FilaEncadeada () {
+		lista = new ListaEncadeadaGenerica<T>();
+		count =0;
+		
+	}
+	
+	public void fazVazia() {
+		lista.fazVazia();
+		count=0;
+	}
+	
+	public boolean estaVazia() {
+		return (count==0);
+	}
+	
+    public T getPrimeiro () throws ContainerVazioException, ListaVaziaException {
+    	if(count==0) {
+    		throw new ContainerVazioException();
+    	}
+    	return lista.getPrimeiro();
+    	
+    }
+    public void enfileirar (T objeto){
+    	lista.inserirFim(objeto);
+    	count++;
+    }
+    
+    public T desinfileirar () throws ContainerVazioException, ObjetoNaoEncontradoException, ListaVaziaException {
+    	if(count==0) {
+    		throw new ContainerVazioException();
+    	}
+    	T result = lista.getPrimeiro(); 
+    	lista.extrair(result);
+    	--count;
+    	return result;
+    }
+ 
+}
