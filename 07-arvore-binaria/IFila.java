1782e0a66d828649be55facca8256d45fdf087cf
Emerson Jesus <emersomjesus999@gmail.com>
2026-03-04 21:19:17 -0300

    Create IFila.java

---
+++ 07-arvore-binaria/IFila.java
@@ -0,0 +1,11 @@
+package atv11_ArvoreBinaria;
+
+public interface IFila <T>{
+	void fazVazia();
+	boolean estaVazia();
+	T getPrimeiro() throws ContainerVazioException, ListaVaziaException;
+	void enfileirar(T objeto);
+	T desinfileirar() throws ContainerVazioException, ObjetoNaoEncontradoException, ListaVaziaException;
+	
+
+}
