71a5470eb66e4a19e42f16306dbc717a2e582737
Emerson Jesus <emersomjesus999@gmail.com>
2026-03-04 21:33:45 -0300

    Create Node.java

---
+++ 08-arvore-binaria-de-busca/Node.java
@@ -0,0 +1,51 @@
+package atv11_ArvoreBinariaDeBusca;
+
+public class Node<T extends Comparable<T>> {
+	private T valor;
+	private Node<T> filhoEsquerda;
+	private Node<T> filhoDireita;
+	
+	//fornecer subArvores no construtor
+	public Node(T valor, Node<T> filhoEsquerda, Node<T> filhoDireita) {
+		this.valor = valor;
+		this.filhoEsquerda = filhoEsquerda ;
+		this.filhoDireita = filhoDireita;
+	}
+	//construtor para arvores sem subArvores
+	public Node(T valor) {
+		this.valor = valor;
+		this.filhoEsquerda = null;
+		this.filhoDireita = null;
+	}
+	public Node() {
+		
+	}
+
+	public T getValor() {
+		return valor;
+	}
+
+	public void setValor(T valor) {
+		this.valor = valor;
+	}
+
+	public Node<T> getFilhoEsquerda() {
+		return filhoEsquerda;
+	}
+
+	public void setFilhoEsquerda(Node<T> filhoEsquerda) {
+		this.filhoEsquerda = filhoEsquerda;
+	}
+
+	public Node<T> getFilhoDireita() {
+		return filhoDireita;
+	}
+
+	public void setFilhoDireita(Node<T> filhoDireita) {
+		this.filhoDireita = filhoDireita;
+	}
+	
+	
+	
+
+}
