b7c1ac6793626655ea72dd605d5e93f5c82d46fd
Emerson Jesus <emersomjesus999@gmail.com>
2026-03-04 21:48:24 -0300

    Create ObjetoNaoEncontradoException.java

---
+++ fila/ObjetoNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package atv6_FilaComListaEncadeada;
+
+public class ObjetoNaoEncontradoException extends Exception {
+	 public ObjetoNaoEncontradoException() {
+	        super("O objeto não foi encontrado na lista!");
+	    }
+
+}
